46a8fafc78382064c6e30d4e6dbbc9957f97fa10
Tanner Patterson <tpatterson5492@gmail.com>
2020-03-15 13:26:45 -0700

    got edit modal button to span length of card and edit modal is rendering, but still not patching

--- src/EditPetForm.js
+++ src/EditPetForm.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import { Button, Form, Header, Image, Modal } from 'semantic-ui-react'
-
+import React from "react";
+import { Button, Form, Header, Image, Modal } from "semantic-ui-react";
 
 const INITIAL_STATE = {
-    name: "",
-    image: "",
-    anmial_type: "",
-    age: "",
-//    owner_id: ""
-  };
+  name: "",
+  image: "",
+  anmial_type: "",
+  age: ""
+  //    owner_id: ""
+};
 class EditPetForm extends React.Component {
-//testing modal click functionality
-  state = { open: false }
+  //testing modal click functionality
+  state = { open: false };
   closeConfigShow = (closeOnEscape, closeOnDimmerClick) => () => {
-    this.setState({ closeOnEscape, closeOnDimmerClick, open: true })
-  }
-  close = () => this.setState({ open: false })
-//-------------------------------------------
-    state = INITIAL_STATE;
-    handleChange = e => {
-          this.setState({ [e.target.name]: e.target.value })
-      };
-      handleEditButton = e => {
-        // debugger;
-        e.preventDefault();
-        this.props.editPet(this.state);
-        this.setState(INITIAL_STATE);
-      };
+    this.setState({ closeOnEscape, closeOnDimmerClick, open: true });
+  };
+  close = () => this.setState({ open: false });
+  //-------------------------------------------
+  state = INITIAL_STATE;
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleEditButton = e => {
+    // debugger;
+    e.preventDefault();
+    this.props.editPet(this.state);
+    this.setState(INITIAL_STATE);
+  };
   render() {
+    console.log("hit edit pet form");
     //testing modal:
-    const { open, closeOnEscape, closeOnDimmerClick } = this.state
-    return(
-<div>
+    const { open, closeOnEscape, closeOnDimmerClick } = this.state;
+    return (
+      <div>
+        <Button onClick={this.closeConfigShow(false, true)} id="edit">
+          Edit
+        </Button>
         <Modal
           open={open}
           closeOnEscape={closeOnEscape}
@@ -43,39 +46,63 @@ class EditPetForm extends React.Component {
             <p>Please update info about your pet</p>
           </Modal.Content>
           <form className="ui form" onSubmit={this.handleEditButton}>
-      <div className="field">
-        <label>Name</label>
-        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} placeholder="Name" />
-      </div>
-      <div className="field">
-        <label>Profile Image</label>
-        <input type="text" name="image" value={this.state.image} onChange={this.handleChange} placeholder="Image" />
-      </div>
-      <div className="field">
-        <label>Anmial Type</label>
-        <input type="text" name="anmial_type" value={this.state.anmial_type} onChange={this.handleChange} placeholder="Animal Type" />
-      </div>
-      <div className="field">
-        <label>Age</label>
-        <input type="text" name="age" value={this.state.age} onChange={this.handleChange} placeholder="age" />
-      </div>
-      {/* <button type="submit" className="ui button">Submit</button> */}
-    </form>
+            <div className="field">
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                placeholder="Name"
+              />
+            </div>
+            <div className="field">
+              <label>Profile Image</label>
+              <input
+                type="text"
+                name="image"
+                value={this.state.image}
+                onChange={this.handleChange}
+                placeholder="Image"
+              />
+            </div>
+            <div className="field">
+              <label>Anmial Type</label>
+              <input
+                type="text"
+                name="anmial_type"
+                value={this.state.anmial_type}
+                onChange={this.handleChange}
+                placeholder="Animal Type"
+              />
+            </div>
+            <div className="field">
+              <label>Age</label>
+              <input
+                type="text"
+                name="age"
+                value={this.state.age}
+                onChange={this.handleChange}
+                placeholder="age"
+              />
+            </div>
+            {/* <button type="submit" className="ui button">Submit</button> */}
+          </form>
           <Modal.Actions>
             <Button onClick={this.close} negative>
-            Close
+              Close
             </Button>
             <Button
               onClick={this.handleFormSubmit}
               positive
-              labelPosition='right'
-              icon='checkmark'
-              content='Submit'
+              labelPosition="right"
+              icon="checkmark"
+              content="Submit"
             />
           </Modal.Actions>
         </Modal>
       </div>
-    )
+    );
   }
 }
-export default EditPetForm
+export default EditPetForm;
--- src/PetCard.js
+++ src/PetCard.js
@@ -1,49 +1,47 @@
-import React from 'react'
-import { Card, Icon, Image, Button } from 'semantic-ui-react'
-import EditPetForm from './EditPetForm'
-
-
+import React from "react";
+import { Card, Icon, Image, Button } from "semantic-ui-react";
+import EditPetForm from "./EditPetForm";
 
 class PetCard extends React.Component {
-// handleDeleteButton = () => {
+  // handleDeleteButton = () => {
   //   props.deletePet(props.pet)
   // }
   //testing modal click functionality
-  state = { open: false }
+  state = { open: false };
   closeConfigShow = (closeOnEscape, closeOnDimmerClick) => () => {
-    this.setState({ closeOnEscape, closeOnDimmerClick, open: true })
-  }
-  close = () => this.setState({ open: false })
-//-------------------------------------------
+    this.setState({ closeOnEscape, closeOnDimmerClick, open: true });
+  };
+  close = () => this.setState({ open: false });
+  //-------------------------------------------
 
   handleDeleteButton = () => {
-    this.props.deletePet(this.props.pet)
-    console.log("DELETE BUTTON CLICKED!!!")
-    console.log(this.props)
-  }
+    this.props.deletePet(this.props.pet);
+    console.log("DELETE BUTTON CLICKED!!!");
+    console.log(this.props);
+  };
 
   render() {
-    const { open, closeOnEscape, closeOnDimmerClick } = this.state
-    return(
+    const { open, closeOnEscape, closeOnDimmerClick } = this.state;
+    return (
       <Card>
         {console.log(this.props.pet.name)}
         <Image src={this.props.pet.image} wrapped ui={false} />
         <Card.Content>
-<Card.Header>{this.props.pet.name}</Card.Header>
+          <Card.Header>{this.props.pet.name}</Card.Header>
           <Card.Meta>
-            <span className='date'>{this.props.pet.animal_type}</span>
+            <span className="date">{this.props.pet.animal_type}</span>
           </Card.Meta>
-          <Card.Description>
-            {this.props.pet.age}
-          </Card.Description>
+          <Card.Description>{this.props.pet.age}</Card.Description>
         </Card.Content>
-        <button className="ui button" onClick={this.closeConfigShow(false, true)}>Edit Pet</button>
-        <EditPetForm editPet={this.props.editPet}/>
-        <button className="ui button" onClick={this.handleDeleteButton}>Delete Pet</button>
-        
+        <>
+          <EditPetForm editPet={this.props.editPet} />
+        </>
+        <button className="ui button" onClick={this.handleDeleteButton}>
+          Delete Pet
+        </button>
       </Card>
-      )
-    }
+    );
+  }
 }
 
-export default PetCard
+export default PetCard;
